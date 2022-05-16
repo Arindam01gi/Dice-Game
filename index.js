2768e792976748fe6a22e3dde329e3c90f1e6829
Arindam Maiti <76761507+Arindam01gi@users.noreply.github.com>
2022-05-16 13:11:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,5 +14,15 @@ var randomNumber2 = Math.floor(Math.random()*6)+1;
 
 var randomImageSource2  ="images/dice"+randomNumber2+".png";
 
-
  document.querySelectorAll("img")[1].setAttribute("src",randomImageSource2);
+
+ // if player1 win
+ if(randomNumber1>randomNumber2){
+     document.querySelector("h1").innerHTML ="🚩 Player1 Wins";
+ }
+ else if (randomNumber2>randomNumber1){
+    document.querySelector("h1").innerHTML ="🚩 Player2 Wins";
+ }
+ else {
+    document.querySelector("h1").innerHTML ="Draw";
+ }
